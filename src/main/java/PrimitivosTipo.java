dfc7bfb9c35bb1d3295c13fed12a1c9a62529a40
lorenamucheniski <lorenamucheniski26@gmail.com>
2021-03-15 20:37:42 -0300

    first commit

---
+++ src/main/java/PrimitivosTipo.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lorena
+ */
+public class PrimitivosTipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno:");
+        String nome = teclado.nextLine();
+        System.out.println("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        System.out.printf("A nota de %s é  %.1f \n ", nome, nota);
+        
+        
+    }
+    
+}
